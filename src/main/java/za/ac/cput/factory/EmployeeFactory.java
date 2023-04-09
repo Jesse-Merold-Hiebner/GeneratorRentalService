5fed9461cfe03761135a2dbf3468f7b0322038a9
Lutho <lnondala07@gmail.com>
2023-04-09 20:18:20 +0200

    Relationships part 4 Commit(Factory Tests)

--- src/main/java/za/ac/cput/factory/EmployeeFactory.java
+++ src/main/java/za/ac/cput/factory/EmployeeFactory.java
@@ -19,11 +19,11 @@ public class EmployeeFactory {
         if (Helper.isNullorEmpty(contactNo)){
             throw new IllegalArgumentException("Contact number is required!");
         }
-        if (job == null){
-            throw new IllegalArgumentException("Job is required!");
-        }
         if (address == null){
             throw new IllegalArgumentException("Contact number is required!");
+        }
+        if (job == null){
+            throw new IllegalArgumentException("Job is required!");
         }
 
         return new Employee.Builder().employeeId(employeeId).firstName(firstName).lastName(lastName).contactNo(contactNo).address(address).job(job).build();
---
+++ src/test/java/za/ac/cput/factory/CalloutServiceFactoryTest.java
@@ -0,0 +1,23 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.CalloutService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalloutServiceFactoryTest {
+    private CalloutService calloutService;
+
+    @Test
+    void create(){
+        Date date = new GregorianCalendar(2023, Calendar.APRIL, 7).getTime();
+        calloutService = CalloutServiceFactory.createCalloutService(1, date);
+        assertNotNull(calloutService);
+    }
+}
